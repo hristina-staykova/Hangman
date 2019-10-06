8518313709fa1d568b04b88576487b8fd6ffb4b9
Hristina Staykova <hristina.staykova@gmail.com>
2019-10-06 20:27:08 +0200

    console.log removed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,10 @@ var compList = ["queen", "nirvana", "muse", "metallica", "coldplay"];
 
 function newGame() {
   var compChoice = compList[Math.floor(Math.random() * compList.length)];
-  console.log(compChoice);
   var hiddenWord = Array(compChoice.length);
   for (i = 0; i < hiddenWord.length; i++) {
     hiddenWord[i] = "_";
   }
-  console.log(hiddenWord);
   var displayWord = hiddenWord.join(" ");
   var guessList = [];
   left = 13;
@@ -26,7 +24,6 @@ function newGame() {
     for (i = 0; i < compChoiceArray.length; i++) {
       if (userInput === compChoiceArray[i]) {
         hiddenWord[i] = userInput;
-        console.log(hiddenWord);
         displayWord = hiddenWord.join(" ");
         document.getElementById("hidden").innerHTML = displayWord;
       }
@@ -41,7 +38,6 @@ function newGame() {
     if (!compChoice.includes(userInput) && !guessList.includes(userInput)) {
       guessList.push(userInput);
       left = left - 1;
-      console.log(left);
       document.getElementById("user-list").innerHTML = guessList;
       document.getElementById("left").innerHTML = left;
       if (left === 0) {
